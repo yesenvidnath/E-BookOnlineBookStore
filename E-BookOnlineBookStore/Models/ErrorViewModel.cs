df33d980ccd3ed7c521bd2596dfa6d6add1fdc5c
yesenvidnath <ykandalama@gmail.com>
2024-11-03 04:32:50 +0530

    The application's .Net version is updated and the application was modifyed

---
+++ E-BookOnlineBookStore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace E_BookOnlineBookStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
