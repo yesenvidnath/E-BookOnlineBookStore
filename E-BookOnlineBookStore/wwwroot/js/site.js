b702a1750fdf239ff1399304f652fe98e9420045
yesenvidnath <ykandalama@gmail.com>
2025-01-02 20:04:27 +0530

    The Front end for the normal process has been created and working

--- E-BookOnlineBookStore/wwwroot/js/site.js
+++ E-BookOnlineBookStore/wwwroot/js/site.js
@@ -2,3 +2,14 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+// Toggle Password Visibility
+document.getElementById('togglePassword').addEventListener('click', function () {
+    const passwordField = document.getElementById('loginPassword');
+    const passwordFieldType = passwordField.getAttribute('type') === 'password' ? 'text' : 'password';
+    passwordField.setAttribute('type', passwordFieldType);
+
+    // Toggle icon class
+    this.querySelector('i').classList.toggle('bi-eye');
+    this.querySelector('i').classList.toggle('bi-eye-slash');
+});
