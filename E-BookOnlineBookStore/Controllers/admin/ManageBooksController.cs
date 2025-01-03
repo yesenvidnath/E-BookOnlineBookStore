e7f1a3f7c93066286307f730c4ef536e990d25d6
GayendriLalithya <rajanayakegayendri@gmail.com>
2025-01-03 19:49:42 +0530

    Manage Books is created the books can be insert update and delete by the admin

--- E-BookOnlineBookStore/Controllers/admin/ManageBooksController.cs
+++ E-BookOnlineBookStore/Controllers/admin/ManageBooksController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using E_BookOnlineBookStore.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
 using System.Data;
 
@@ -30,6 +31,75 @@ namespace E_BookOnlineBookStore.Controllers.Admin
             }
 
             return View("~/Views/Account/Admin/Books.cshtml", booksTable);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] Book newBook)
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                string query = "INSERT INTO Books (Title, Author, ISBN, Description, Price, CategoryID, StockQuantity, CreatedAt, UpdatedAt) " +
+                               "VALUES (@Title, @Author, @ISBN, @Description, @Price, @CategoryID, @StockQuantity, GETDATE(), GETDATE())";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Title", newBook.Title);
+                    command.Parameters.AddWithValue("@Author", newBook.Author ?? (object)DBNull.Value);
+                    command.Parameters.AddWithValue("@ISBN", newBook.Isbn ?? (object)DBNull.Value);
+                    command.Parameters.AddWithValue("@Description", newBook.Description ?? (object)DBNull.Value);
+                    command.Parameters.AddWithValue("@Price", newBook.Price);
+                    command.Parameters.AddWithValue("@CategoryID", newBook.CategoryId ?? (object)DBNull.Value);
+                    command.Parameters.AddWithValue("@StockQuantity", newBook.StockQuantity);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Update([FromBody] Book updatedBook)
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                string query = "UPDATE Books " +
+                               "SET Title = @Title, Author = @Author, ISBN = @ISBN, Description = @Description, " +
+                               "Price = @Price, CategoryID = @CategoryID, StockQuantity = @StockQuantity, UpdatedAt = GETDATE() " +
+                               "WHERE BookID = @BookID";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@BookID", updatedBook.BookId);
+                    command.Parameters.AddWithValue("@Title", updatedBook.Title);
+                    command.Parameters.AddWithValue("@Author", updatedBook.Author ?? (object)DBNull.Value);
+                    command.Parameters.AddWithValue("@ISBN", updatedBook.Isbn ?? (object)DBNull.Value);
+                    command.Parameters.AddWithValue("@Description", updatedBook.Description ?? (object)DBNull.Value);
+                    command.Parameters.AddWithValue("@Price", updatedBook.Price);
+                    command.Parameters.AddWithValue("@CategoryID", updatedBook.CategoryId ?? (object)DBNull.Value);
+                    command.Parameters.AddWithValue("@StockQuantity", updatedBook.StockQuantity);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                string query = "DELETE FROM Books WHERE BookID = @BookID";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@BookID", id);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+            return Ok();
         }
 
     }
